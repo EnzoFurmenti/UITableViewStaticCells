fc44f55da92e7adb19506965580867086f77ca0e
EnzoFurmenti <ducheenzo@mail.ru>
2016-09-11 14:42:44 +0300

    Initial Commit

---
+++ UITableViewStaticCells/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITableViewStaticCells
+//
+//  Created by EnzoF on 11.09.16.
+//  Copyright © 2016 EnzoF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UITableViewStaticCells/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableViewStaticCells
+//
+//  Created by EnzoF on 11.09.16.
+//  Copyright © 2016 EnzoF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
